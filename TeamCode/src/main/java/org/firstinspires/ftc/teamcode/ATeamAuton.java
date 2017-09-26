27003a675c44db7fcaaef66f32756e9584ec7d2b
FTCRobocats-ATeam-Mr.G <woodrowrobcats56@gmail.com>
2017-09-26 16:24:37 -0500

    hi

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ATeamAuton.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ATeamAuton.java
@@ -7,5 +7,15 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
 @Autonomous (name="Auton Alpha 1.0", group="Autonomous")
-public class ATeamAuton {
+public abstract class ATeamAuton {
+    /*
+    Close grippers onto glyph.
+    Lift up glyph.
+    Place glyph on top of another glyph.
+    Open and lower gripper.
+    Close and grip both glyphs.
+    Lift both glyphs.
+    Drive to CryptoBox.
+    Open gripper and drop off glyphs into CryptoBox.
+     */
 }
