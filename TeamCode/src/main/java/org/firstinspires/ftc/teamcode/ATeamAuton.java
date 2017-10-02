89b325412a8aafc20af23c0d929d933c7e8bc3ba
FTCRobocats-ATeam-Mr.G <woodrowrobcats56@gmail.com>
2017-10-02 18:55:07 -0500

    ATeamAuton1.1.2
    Add the function to be able to drive straight for 3 secs and turn for 3 secs

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ATeamAuton.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ATeamAuton.java
@@ -12,8 +12,6 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 @Autonomous (name="Auton Alpha 1.0", group="Autonomous")
 public class ATeamAuton extends LinearOpMode {
 
-
-
     public static final double COUNTS_PER_MOTOR_REV = 1220;
     public static final double DRIVE_GEAR_REDUCTION = 1.0;
     public static final double WHEEL_DIAMETER_INCHES = 4.0;
@@ -35,7 +33,7 @@ public class ATeamAuton extends LinearOpMode {
         leftDrive2  = hardwareMap.get(DcMotor.class, "leftDrive2");
         rightDrive2 = hardwareMap.get(DcMotor.class, "rightDrive2");
 
-        leftDrive1.setDirection(DcMotor.Direction.FORWARD);
+        leftDrive1.setDirection(DcMotor.Direction.REVERSE);
         rightDrive1.setDirection(DcMotor.Direction.FORWARD);
         leftDrive2.setDirection(DcMotor.Direction.FORWARD);
         rightDrive2.setDirection(DcMotor.Direction.REVERSE);
@@ -58,24 +56,28 @@ public class ATeamAuton extends LinearOpMode {
 
         waitForStart();
 
-        encoderDrive(DRIVE_SPEED,  36,  36, 5);       //(Left Wheel Distance (IN.), Right-Wheel Distance, Timeout (Sec))
-
+        encoderDrive(DRIVE_SPEED, 24, 24,3);       //(Left Wheel Distance (IN.), Right-Wheel Distance, Timeout (Sec))
+        encoderDrive(DRIVE_SPEED, 24, -24, 3);
 
     }
 
     public void encoderDrive(double speed, double leftInches, double rightInches, double timeoutS) {
 
-        int newLeftTarget;
-        int newRightTarget;
+        int newLeft1Target;
+        int newRight1Target;
+        int newLeft2Target;
+        int newRight2Target;
 
         if (opModeIsActive()) {
 
-            newLeftTarget = leftDrive1.getCurrentPosition() + (int) (leftInches * COUNTS_PER_INCH);
-            newRightTarget = rightDrive2.getCurrentPosition() + (int) (rightInches * COUNTS_PER_INCH);
-            leftDrive1.setTargetPosition(newLeftTarget);
-            rightDrive1.setTargetPosition(newRightTarget);
-            leftDrive2.setTargetPosition(newLeftTarget);
-            rightDrive2.setTargetPosition(newRightTarget);
+            newLeft1Target = leftDrive1.getCurrentPosition() + (int) (leftInches * COUNTS_PER_INCH);
+            newRight1Target = rightDrive2.getCurrentPosition() + (int) (rightInches * COUNTS_PER_INCH);
+            newLeft2Target = leftDrive1.getCurrentPosition() + (int) (leftInches * COUNTS_PER_INCH);
+            newRight2Target = rightDrive2.getCurrentPosition() + (int) (rightInches * COUNTS_PER_INCH);
+            leftDrive1.setTargetPosition(newLeft1Target);
+            rightDrive1.setTargetPosition(newRight1Target);
+            leftDrive2.setTargetPosition(newLeft2Target);
+            rightDrive2.setTargetPosition(newRight2Target);
 
             leftDrive1.setMode(DcMotor.RunMode.RUN_TO_POSITION);
             rightDrive1.setMode(DcMotor.RunMode.RUN_TO_POSITION);
